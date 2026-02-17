2d7ae737d1f6873f7df21106963b0d29fbb11b9c
unknown <shahajalalsheikh001@gmail.com>
2026-02-17 20:47:00 +0600

    finished the assaignment and deploying in netlify

--- script/app.js
+++ script/app.js
@@ -27,6 +27,29 @@ const loadData = async () => {
   displayData(products);
 };
 
+const loadProductDetails = async (id) => {
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const details = await res.json();
+  displayProductDetails(details);
+};
+
+const displayProductDetails = (product) => {
+  console.log(product);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `
+  <div class="space-y-3">
+  <img src=${product.image} alt="">
+  <h2 class="text-2xl">${product.title}</h2>
+  <p>${product.description}</p>
+  <div>
+    <button class="btn bg-gray-300 hover:bg-blue-700 transition duration-500">Order Now</button>
+    <button class="btn bg-blue-700 hover:bg-gray-300 transition duration-500">Add To Cart</button>
+  </div>
+</div>
+  `;
+  document.getElementById("my_modal").showModal();
+};
+
 const displayData = (products) => {
   console.log(products);
   const productContainer = document.getElementById("productContainer");
@@ -52,7 +75,7 @@ const displayData = (products) => {
               </h2>
               <p class="text-2xl font-bold"> $ ${product.price}</p>
               <div class="flex justify-between text-gray-300">
-                <button onclick="my_modal_5.showModal()"
+                <button onclick="loadProductDetails(${product.id})"
                   class="btn hover:bg-blue-700 transition duration-500"
                 >
                   <i class="fa fa-eye" aria-hidden="true"></i>Details
